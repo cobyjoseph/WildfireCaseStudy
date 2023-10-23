290938433e053ea5b5fb2f4867bb8a48f788307f
Coby Joseph <coby.joseph@gmail.com>
2023-10-23 01:39:34 -0400

    almost there

--- src/routes/post/[slug]/+page.svelte
+++ src/routes/post/[slug]/+page.svelte
@@ -35,41 +35,12 @@
 
 <svelte:head>
   <title>{data.post.title} - {name}</title>
-  <meta name="description" content={data.post.preview.text} />
-  <meta name="author" content={name} />
-
-  <!-- Facebook Meta Tags -->
-  <meta property="og:url" content={url} />
-  <meta property="og:type" content="website" />
-  <meta property="og:title" content={data.post.title} />
-  <meta property="og:description" content={data.post.preview.text} />
-  <meta property="og:image" content={ogImage} />
-
-  <!-- Twitter Meta Tags -->
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta property="twitter:domain" content={website} />
-  <meta property="twitter:url" content={url} />
-  <meta name="twitter:title" content={data.post.title} />
-  <meta name="twitter:description" content={data.post.preview.text} />
-  <meta name="twitter:image" content={ogImage} />
+  
 </svelte:head>
 
 <div class="root max-w-2xl mx-auto lg:max-w-none">
   <div class="hidden lg:block pt-8">
-    <div class="sticky top-0 w-full flex justify-end pt-11 pr-8">
-      <svelte:element
-        this={canGoBack ? 'button' : 'a'}
-        class="items-center justify-center hidden w-10 h-10 mb-8 transition bg-white rounded-full shadow-md -top-1 -left-16 lg:flex group shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0 dark:focus-visible:ring-2 dark:ring-white/10 dark:hover:border-zinc-700 dark:hover:ring-white/20"
-        href={canGoBack ? undefined : '/posts'}
-        aria-label="Go back to posts"
-        on:click={goBack}
-        on:keydown={goBack}
-      >
-        <ArrowLeftIcon
-          class="w-4 h-4 transition stroke-zinc-500 group-hover:stroke-zinc-700 dark:stroke-zinc-500 dark:group-hover:stroke-zinc-400"
-        />
-      </svelte:element>
-    </div>
+    
   </div>
 
   <div class="w-full mx-auto overflow-x-hidden">
@@ -90,26 +61,8 @@
     </article>
 
     <!-- bio -->
-    <hr />
-    <div class="py-8">
-      <div class="grid gap-8">
-        <div class="flex justify-center order-1 col-span-2 gap-6 md:order-2">
-          <SocialLinks />
-        </div>
-        <div class="flex justify-center order-2 md:order-1 md:col-span-2">
-          <a href="/" class="inline-block rounded-full">
-            <img
-              src={avatar}
-              alt={name}
-              class="w-24 h-24 mx-auto rounded-full md:w-28 md:h-28 ring-2 ring-zinc-200 dark:ring-zinc-700"
-            />
-          </a>
-        </div>
-        <p class="order-3 text-base text-zinc-600 dark:text-zinc-400">
-          {bio}
-        </p>
-      </div>
-    </div>
+   
+   
   </div>
 
   <!-- table of contents -->
