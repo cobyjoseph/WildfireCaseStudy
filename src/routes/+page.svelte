ddcad629a9f959e71cebe6e0bf7b3c49e3e2c2f1
Coby Joseph <coby.joseph@gmail.com>
2023-10-23 01:30:54 -0400

    working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,10 @@
 <script>
-  import ArrowRightIcon from '$lib/components/ArrowRightIcon.svelte'
-  import PostsList from '$lib/components/PostsList.svelte'
-  import SocialLinks from '$lib/components/SocialLinks.svelte'
-  import { avatar, bio, name } from '$lib/info.js'
-  import caseStudy from '$lib/caseStudy.md'
-  import mdsvex from 'mdsvex'
+
 
   /** @type {import('./$types').PageData} */
-  export let data
+  
 </script>
 
-<svelte:head>
-  <title>{name}</title>
-</svelte:head>
 
 
-<svelte:component this={caseStudy} />
+<a href="/post/case-study">Case study here</a>
--- src/routes/posts/[[page]]/+page.svelte
+++ src/routes/posts/[[page]]/+page.svelte
@@ -1,7 +1,10 @@
 <script>
   import { name } from '$lib/info.js'
+  import index from '$lib/caseStudy.md'
 </script>
 
 <svelte:head>
   <title>{name} | Posts</title>
 </svelte:head>
+
+<a href="../post/case-study">Case study here</a>
